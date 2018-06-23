c44b33f3a14efed165bb1c3949802318404202e7
Andrey Cherkasov <andreychas1@gmail.com>
2018-06-23 12:18:13 +0300

    added comment to AllTests

--- test/AllTests.java
+++ test/AllTests.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         SjavacTest.class,
         LineFactoryTest.class
+        // put other test classes here
 })
 public class AllTests {
 }
