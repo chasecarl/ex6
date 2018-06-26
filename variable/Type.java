e47ac28f42e827425e662d88930607e8811af511
Andrey Cherkasov <andreychas1@gmail.com>
2018-06-26 18:00:17 +0300

    trash was cleaned

--- variable/Type.java
+++
@@ -1,8 +0,0 @@
-package variable;
-
-public enum Type {
-    INTEGER { public String toString() { return "int"; }},
-    DOUBLE { public String toString() {return "double"; }};
-
-    public abstract String toString();
-}
--- variable/Variable.java
+++
@@ -1,4 +0,0 @@
-package variable;
-
-public interface Variable {
-}
